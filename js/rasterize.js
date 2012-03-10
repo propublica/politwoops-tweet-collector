c55435caf94a8180065005b52000028b494509a4
Eric Mill <kprojection@gmail.com>
2012-03-10 15:01:32 -0500

    Following redirects in phantomjs

--- js/rasterize.js
+++ js/rasterize.js
@@ -9,15 +9,34 @@ if (phantom.args.length < 2 || phantom.args.length > 3) {
     output = phantom.args[1];
     page.viewportSize = { width: 1280, height: 1024 };
     page.clipRect = { width: 1280, height: 1024 };
+    
+    var retries = 0;
+    var deathClock = false;
+
     page.open(address, function (status) {
-        if (status !== 'success') {
-            console.log('Unable to load the address!');
+        if (status == 'success') {
             window.setTimeout(function () {
-                phantom.exit();
+                if (page.render(output)) {
+                    console.log("Rendered results, stopping");
+                    phantom.exit();
+                } else if (retries < 5) {
+                    retries += 1;
+                    console.log("Didn't render, assuming a redirect, waiting for a new callback (#" + retries + ")");
+                    if (!deathClock) {
+                        deathClock = true;
+                        console.log("Setting death clock of 7 seconds, in case this isn't a redirect");
+                        window.setTimeout(function() {
+                            phantom.exit();
+                        }, 7000);
+                    }
+                } else {
+                    console.log("Too many retries (" + retries + "), giving up");
+                    phantom.exit();
+                }
             }, 200);
         } else {
+            console.log('Unable to load the address!');
             window.setTimeout(function () {
-                page.render(output);
                 phantom.exit();
             }, 200);
         }
