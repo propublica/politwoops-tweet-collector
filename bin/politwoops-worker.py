ac69c1284568928d018b41a3fa930b68f0d67e70
Eric Mill <konklone@gmail.com>
2012-08-17 20:12:54 -0400

    Oops, forgot how python dicts work

--- bin/politwoops-worker.py
+++ bin/politwoops-worker.py
@@ -161,7 +161,13 @@ class DeletedTweetsWorker:
                 if self.images and tweet.has_key('entities'):
                     tweet_id = tweet['id']
 
-                    for i, url_entity in enumerate(tweet['entities']['urls'] + tweet['entities']['media']):
+                    entities = []
+                    if tweet['entities'].has_key('urls'):
+                        entities = entities + tweet['entities']['urls']
+                    if tweet['entities'].has_key('media'):
+                        entities = entities + tweet['entities']['media']
+
+                    for i, url_entity in enumerate(entities):
                         origin_url = url_entity['expanded_url']
                         filename = "%i-%i.png" % (tweet_id, i)
 
