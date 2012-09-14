4bb5a585ef337dc61473067a70d1321e8275be6f
Drew Vogel <dvogel@sunlightfoundation.com>
2012-09-14 17:42:19 -0400

    Rearranged log handlers to reduce complexity of the worker class. Tweaked levels of each log message.

--- bin/politwoops-worker.py
+++ bin/politwoops-worker.py
@@ -61,9 +61,7 @@ class Usage(Exception):
 
 
 class DeletedTweetsWorker:
-    def __init__(self, verbose, output, images):
-        self.verbose = verbose
-        self.output = output
+    def __init__(self, images):
         self.images = images
         self.get_config()
     
@@ -84,7 +82,7 @@ class DeletedTweetsWorker:
     
     def get_beanstalk(self):
         tube = self.config.get('beanstalk', 'tube')
-        log.debug("Initiating beanstalk connection for tube {tube}...", tube=tube)
+        log.info("Initiating beanstalk connection for tube {tube}...", tube=tube)
         beanstalk = politwoops.utils.beanstalk(host=self.config.get('beanstalk', 'host'), port=int(self.config.get('beanstalk', 'port')), tube=self.config.get('beanstalk', 'tube'))
         return beanstalk
 
@@ -101,26 +99,22 @@ class DeletedTweetsWorker:
         for t in cursor.fetchall():
             ids[t[0]] = t[2]
             politicians[t[0]] = t[1]
-        log.debug("Found ids: {ids}", ids=ids)
-        log.debug("Found politicians: {politicians}", politicians=politicians)
+        log.info("Found ids: {ids}", ids=ids)
+        log.info("Found politicians: {politicians}", politicians=politicians)
         return ids, politicians
 
     def run(self):
         mimetypes.init()
-        log_handler = logbook.SyslogHandler(application_name='politwoops-worker',
-                                            bubble=self.verbose,
-                                            level='DEBUG' if self.verbose else 'INFO')
-        with log_handler.applicationbound():
-            self.database = self.get_database()
-            self.beanstalk = self.get_beanstalk()
-            self.users, self.politicians = self.get_users()
-            self.stathat = self.get_stathat()
-            while True:
-                sleep(0.2)
-                job = self.beanstalk.reserve(timeout=0)
-                if job:
-                    self.handle_tweet(job.body)
-                    job.delete()
+        self.database = self.get_database()
+        self.beanstalk = self.get_beanstalk()
+        self.users, self.politicians = self.get_users()
+        self.stathat = self.get_stathat()
+        while True:
+            sleep(0.2)
+            job = self.beanstalk.reserve(timeout=0)
+            if job:
+                self.handle_tweet(job.body)
+                job.delete()
 
     def run_with_restart(self):
         # keeps tabs on whether we should restart the connection to Twitter ourselves
@@ -137,7 +131,7 @@ class DeletedTweetsWorker:
                 sleep(1) # restart after a second
 
                 self.restartCounter += 1
-                log.debug("Some sort of error, restarting for the {nth} time: {exception}",
+                log.error("Some sort of error, restarting for the {nth} time: {exception}",
                           nth=self.restartCounter,
                           exception=str(e))
     
@@ -186,16 +180,16 @@ class DeletedTweetsWorker:
                                      'urls': urls}
 
                         if len(urls) == 0:
-                            log.warn("No URLs for entity {entity} on tweet {tweet}. Skipping.",
+                            log.info("No URLs for entity {entity} on tweet {tweet}. Skipping.",
                                      **log_context)
                             return
 
-                        log.debug("URLs for entity {entity} on tweet {tweet}: {urls}",
+                        log.info("URLs for entity {entity} on tweet {tweet}: {urls}",
                                   **log_context)
 
                         for url in urls:
                             response = requests.head(url)
-                            log.debug("HEAD {status_code} {url} {bytes}",
+                            log.info("HEAD {status_code} {url} {bytes}",
                                       status_code=response.status_code,
                                       url=url,
                                       bytes=len(response.content) if response.content else '')
@@ -211,7 +205,7 @@ class DeletedTweetsWorker:
 
 
     def handle_deletion(self, tweet):
-        log.debug("Deleted tweet {0}", tweet['delete']['status']['id'])
+        log.notice("Deleted tweet {0}", tweet['delete']['status']['id'])
         cursor = self.database.cursor()
         cursor.execute("""SELECT COUNT(*) FROM `tweets` WHERE `id` = %s""", (tweet['delete']['status']['id'],))
         num_previous = cursor.fetchone()[0]
@@ -222,7 +216,7 @@ class DeletedTweetsWorker:
             cursor.execute("""REPLACE INTO `tweets` (`id`, `deleted`, `modified`, `created`) VALUES(%s, 1, NOW(), NOW())""", (tweet['delete']['status']['id']))
     
     def handle_new(self, tweet):
-        log.debug("New tweet {tweet} from user {user_id}/{screen_name}",
+        log.notice("New tweet {tweet} from user {user_id}/{screen_name}",
                   tweet=tweet.get('id'),
                   user_id=tweet.get('user', {}).get('id'),
                   screen_name=tweet.get('user', {}).get('screen_name'))
@@ -244,15 +238,15 @@ class DeletedTweetsWorker:
 
         if num_previous > 0:
             cursor.execute("""UPDATE `tweets` SET `user_name` = %s, `politician_id` = %s, `content` = %s, `tweet`=%s, `modified`= NOW() WHERE id = %s""", (tweet['user']['screen_name'], self.users[tweet['user']['id']], tweet['text'], anyjson.serialize(tweet), tweet['id'],))
-            log.debug("Updated tweet {0}", tweet.get('id'))
+            log.info("Updated tweet {0}", tweet.get('id'))
         else:
             #cursor.execute("""DELETE FROM `tweets` WHERE `id` = %s""", (tweet['id'],))
             cursor.execute("""INSERT INTO `tweets` (`id`, `user_name`, `politician_id`, `content`, `created`, `modified`, `tweet`) VALUES(%s, %s, %s, %s, NOW(), NOW(), %s)""", (tweet['id'], tweet['user']['screen_name'], self.users[tweet['user']['id']], tweet['text'], anyjson.serialize(tweet)))
-            log.debug("Inserted new tweet {0}", tweet.get('id'))
+            log.info("Inserted new tweet {0}", tweet.get('id'))
 
             
         if was_deleted:
-            log.debug("Tweet deleted {0} before it came!", tweet.get('id'))
+            log.warn("Tweet deleted {0} before it came!", tweet.get('id'))
             self.copy_tweet_to_deleted_table(tweet['id'])
         
         self.stathat_add_count('tweets')
@@ -260,7 +254,7 @@ class DeletedTweetsWorker:
     def handle_image(self, tweet, url):
         cursor = self.database.cursor()
         cursor.execute("""INSERT INTO `tweet_images` (`tweet_id`, `url`, `created_at`, `updated_at`) VALUES(%s, %s, NOW(), NOW())""", (tweet['id'], url))
-        log.debug("Inserted image into database for tweet {tweet}: {url}",
+        log.info("Inserted image into database for tweet {tweet}: {url}",
                   tweet=tweet.get('id'), url=url)
     
     def copy_tweet_to_deleted_table(self, tweet_id):
@@ -287,9 +281,9 @@ class DeletedTweetsWorker:
         tmp_path = os.path.join(tmp_dir, filename)
 
         command = "phantomjs js/rasterize.js %s %s" % (url, tmp_path)
-        log.info("Running {0}", command)
+        log.debug("Running {0}", command)
         code = subprocess.call(command, shell=True)
-        log.debug("Command {0} exited with status {1}", command, code)
+        log.info("Command {0} exited with status {1}", command, code)
         if code != 0:
             return
        
@@ -341,14 +335,15 @@ class DeletedTweetsWorker:
             key.set_contents_from_filename(tmp_path,
                                            policy='public-read',
                                            headers={'Content-Type': content_type})
-            log.debug("Upload image {0} to {1}", tmp_path, url)
+            log.notice("Uploaded image {0} to {1}", tmp_path, url)
             return url
         except IOError as e:
             log.warn("Failed to upload image {0} to {1} because {2}", tmp_path, url, str(e))
             return None
 
 def main(argv=None):
-    verbose = False
+    loglevel = logbook.NOTICE
+    loglevel_name = "NOTICE"
     images = False
     output = None
     harden = True
@@ -357,16 +352,23 @@ def main(argv=None):
         argv = sys.argv
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "ho:ivr", ["help", "output=", "images", "raise"])
+            opts, args = getopt.getopt(argv[1:], "hl:o:ivr", ["help", "loglevel=", "output=", "images", "raise"])
         except getopt.error, msg:
             raise Usage(msg)
         
         # option processing
         for option, value in opts:
             if option == "-v":
-                verbose = True
+                raise Usage("The verbose option (-v) is no longer supported. Use the loglevel option (-l) instead.")
             if option in ("-h", "--help"):
                 raise Usage(help_message)
+            if option in ("-l", "--loglevel"):
+                loglevel_name = value.upper()
+                if not hasattr(logbook, loglevel_name):
+                    raise Usage("Invalid {0} value: {1}".format(option, value))
+                loglevel = getattr(logbook, loglevel_name)
+                if not isinstance(loglevel, int):
+                    raise Usage("Invalid {0} value: {1}".format(option, value))
             if option in ("-o", "--output"):
                 output = value
             if option in ("-i", "--images"):
@@ -378,17 +380,35 @@ def main(argv=None):
         print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
         print >> sys.stderr, "\t for help use --help"
         return 2
-    
-    if verbose:
-        log.info("Starting Politwoops worker...")
-    if images:
-        log.info("Screenshot support enabled.")
 
-    app = DeletedTweetsWorker(verbose, output, images)
-    if harden:
-        return app.run_with_restart()
+    if output == 'syslog':
+        log_handler = logbook.SyslogHandler(
+            application_name='politwoops-worker',
+            bubble=False,
+            level=loglevel)
+    elif output == '-' or not output:
+        log_handler = logbook.StderrHandler(
+            level=loglevel,
+            bubble=False)
     else:
-        return app.run()
+        log_handler = logbook.FileHandler(
+            filename=output,
+            encoding='utf-8',
+            level=loglevel,
+            bubble=False)
+
+    with logbook.NullHandler():
+        with log_handler.applicationbound():
+            log.info("Starting Politwoops worker...")
+            log.notice("Log level {0}".format(loglevel_name))
+            if images:
+                log.notice("Screenshot support enabled.")
+
+            app = DeletedTweetsWorker(images)
+            if harden:
+                return app.run_with_restart()
+            else:
+                return app.run()
 
 
 if __name__ == "__main__":
