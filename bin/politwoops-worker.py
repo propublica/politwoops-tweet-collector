bd3dcabfbcb7bfbeff412ff3b17fd67c42cb2b9a
Derek Willis <derek.willis@propublica.org>
2017-12-06 09:11:31 -0500

    get tweet id

--- bin/politwoops-worker.py
+++ bin/politwoops-worker.py
@@ -154,7 +154,7 @@ class DeletedTweetsWorker(object):
         self.copy_tweet_to_deleted_table(tweet['delete']['status']['id'])
 
     def handle_new(self, tweet):
-        tweet_json = self.tweepy_client.get_status(tweet['id'], tweet_mode='extended')._json
+        tweet_json = self.tweepy_client.get_status(tweet.get('id'), tweet_mode='extended')._json
         log.notice("New tweet {tweet} from user {user_id}/{screen_name}",
                   tweet=tweet.get('id'),
                   user_id=tweet.get('user', {}).get('id'),
