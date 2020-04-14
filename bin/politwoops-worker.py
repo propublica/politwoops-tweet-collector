955bb52b86371a6c43259d586ebc741766fd1c16
Derek Willis <derek.willis@propublica.org>
2020-04-13 23:01:39 -0400

    turn off for now

--- bin/politwoops-worker.py
+++ bin/politwoops-worker.py
@@ -199,11 +199,11 @@ class DeletedTweetsWorker(object):
                             retweeted_user_name))
             log.info("Inserted new tweet {0}", tweet.get('id'))
 
-            cursor.execute("""INSERT INTO `twitter_metrics` (`politician_id`, `date`, `followers_count`, `tweets_count`) VALUES (%s, CURDATE(), %s, %s) ON DUPLICATE KEY UPDATE followers_count = VALUES(followers_count), tweets_count = VALUES(tweets_count))""",
-                (self.users[tweet['user']['id']],
-                tweet['user']['followers_count'],
-                tweet['user']['statuses_count']
-                ))
+            #cursor.execute("""INSERT INTO `twitter_metrics` (`politician_id`, `date`, `followers_count`, `tweets_count`) VALUES (%s, CURDATE(), %s, %s) ON DUPLICATE KEY UPDATE followers_count = VALUES(followers_count), tweets_count = VALUES(tweets_count))""",
+            #    (self.users[tweet['user']['id']],
+            #    tweet['user']['followers_count'],
+            #    tweet['user']['statuses_count']
+            #    ))
 
 
         if was_deleted:
